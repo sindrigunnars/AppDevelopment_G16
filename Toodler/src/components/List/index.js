61542f300b844d7623738deb6ed7a3ef90acd373
sindrigunnars <102031392+sindrigunnars@users.noreply.github.com>
2023-11-29 15:39:06 +0000

    initial refactor

--- Toodler/src/components/List/index.js
+++ Toodler/src/components/List/index.js
@@ -1,8 +1,50 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useCallback } from 'react';
 import { TouchableOpacity, Text, View, StyleSheet } from 'react-native';
 import PropTypes from 'prop-types';
 import { DataContext } from '../data';
 import { useNavigation } from '@react-navigation/native';
+
+const IndividualTask = ({ task, listId, toggleTaskFinished, deleteTask, toggleDoubleExpand, doubleExpanded }) => {
+    const navigation = useNavigation();
+    return (
+        <TouchableOpacity style={styles.individualTask} onPress={() => toggleDoubleExpand(task.id)}>
+            <Text>{task.name}</Text>
+            { doubleExpanded === task.id && (
+                <View>
+                    <Text>{task.description}</Text>
+                    <View style={styles.taskButtons}>
+                        {task.isFinished === false
+                            ? (
+                                <TouchableOpacity onPress={() => toggleTaskFinished(task.id)}>
+                                    <Text>Mark Done</Text>
+                                </TouchableOpacity>
+                            )
+                            : (
+                                <TouchableOpacity onPress={() => toggleTaskFinished(task.id)}>
+                                    <Text>Mark Undone</Text>
+                                </TouchableOpacity>
+                            )}
+                        <TouchableOpacity onPress={() => navigation.navigate('Edit Task', { modify: true, task, listId })}>
+                            <Text>Edit</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity onPress={() => deleteTask(task.id)}>
+                            <Text>Delete</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            )}
+        </TouchableOpacity>
+    );
+};
+
+IndividualTask.propTypes = {
+    task: PropTypes.object.isRequired,
+    listId: PropTypes.number.isRequired,
+    toggleTaskFinished: PropTypes.func.isRequired,
+    deleteTask: PropTypes.func.isRequired,
+    toggleDoubleExpand: PropTypes.func.isRequired,
+    doubleExpanded: PropTypes.number
+};
 
 const List = ({ list }) => {
     const { id, name, color } = list;
@@ -10,13 +52,13 @@ const List = ({ list }) => {
     const tasks = data.tasks.filter((task) => task.listId === list.id);
     const [expanded, setExpanded] = useState(false);
     const [doubleExpanded, setDoubleExpanded] = useState(null);
-    const navigation = useNavigation();
+
+    const toggleDoubleExpand = useCallback((index) => {
+        setDoubleExpanded(doubleExpanded === index ? null : index);
+    }, [doubleExpanded, setDoubleExpanded]);
 
     const toggleExpand = () => {
         setExpanded(!expanded);
-    };
-    const toggleDoubleExpand = (index) => {
-        setDoubleExpanded(doubleExpanded === index ? null : index);
     };
 
     const toggleTaskFinished = (taskId) => {
@@ -50,41 +92,18 @@ const List = ({ list }) => {
             <TouchableOpacity style={{ ...styles.list, backgroundColor: color }} onPress={toggleExpand}>
                 <Text style={styles.header}>{name}</Text>
             </TouchableOpacity>
-            <View style={styles.taskList} >
-                {expanded // ------------------------------------------ EXPANDING LIST TO SHOW TASKS
-                    ? tasks.map((task, index) => {
-                        return (
-                            <TouchableOpacity style={styles.individualTask} key={index} onPress={() => toggleDoubleExpand(index)}>
-                                <Text>{task.name}</Text>
-                                {doubleExpanded === index && ( // ----- EXPAuwuNDING TASK TO SHOW TASK INFO AND OPTIONS
-                                    <View>
-                                        <Text>{task.description}</Text>
-                                        <View style={styles.taskButtons}>
-                                            {task.isFinished === false
-                                                ? (
-                                                    <TouchableOpacity onPress={() => toggleTaskFinished(task.id)}>
-                                                        <Text>Mark Done</Text>
-                                                    </TouchableOpacity>
-                                                )
-                                                : (
-                                                    <TouchableOpacity onPress={() => toggleTaskFinished(task.id)}>
-                                                        <Text>Mark Undone</Text>
-                                                    </TouchableOpacity>
-                                                )}
-                                            <TouchableOpacity onPress={() => navigation.navigate('Edit Task', { modify: true, task: task, listId: id })}>
-                                                <Text>Edit</Text>
-                                            </TouchableOpacity>
-                                            <TouchableOpacity onPress={() => deleteTask(task.id)}>
-                                                <Text>Delete</Text>
-                                            </TouchableOpacity>
-                                        </View>
-                                    </View>
-
-                                )}
-                            </TouchableOpacity>
-                        );
-                    })
-                    : null}
+            <View style={styles.taskList}>
+                {expanded && tasks.map((task, index) => (
+                    <IndividualTask
+                        key={index}
+                        task={task}
+                        listId={id}
+                        toggleTaskFinished={toggleTaskFinished}
+                        toggleDoubleExpand={toggleDoubleExpand}
+                        doubleExpanded={doubleExpanded}
+                        deleteTask={deleteTask}
+                    />
+                ))}
             </View>
         </View>
     );
@@ -116,6 +135,7 @@ const styles = StyleSheet.create({
     taskButtons: {
         flex: 1,
         flexDirection: 'row',
+        justifyContent: 'space-between',
         columnGap: '1em'
     }
 });
