ddf953842655aa38b273526bab56a257071c2db2
ThordisME <112634480+ThordisME@users.noreply.github.com>
2023-11-29 12:30:46 +0000

    route for editing task added

--- Toodler/src/routes/index.js
+++ Toodler/src/routes/index.js
@@ -4,6 +4,8 @@ import { createStackNavigator } from '@react-navigation/stack';
 import Boards from '../views/main';
 import Lists from '../views/lists';
 import Tasks from '../views/tasks';
+import ModifyBoard from '../views/modifyBoard';
+import ModifyTask from '../views/modifyTask';
 
 const Stack = createStackNavigator();
 
@@ -11,8 +13,10 @@ const Routes = () => (
     <NavigationContainer>
         <Stack.Navigator initialRouteName="Main">
             <Stack.Screen name="Boards" component={Boards} />
+            <Stack.Screen name="Edit Board" component={ModifyBoard} />
             <Stack.Screen name="Lists" component={Lists} />
             <Stack.Screen name="Tasks" component={Tasks} />
+            <Stack.Screen name="Edit Task" component={ModifyTask} />
         </Stack.Navigator>
     </NavigationContainer>
 );
