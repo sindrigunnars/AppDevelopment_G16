2d439401af79f1947d2d75e7a080c0f823ccee54
sindrigunnars <102031392+sindrigunnars@users.noreply.github.com>
2023-12-02 09:03:57 +0000

    format fix

--- Toodler/App.js
+++ Toodler/App.js
@@ -1,21 +1,14 @@
-import React, { useState } from "react";
-import AppContainer from "./src/routes";
-import { DataContext } from "./src/components/data";
-import jsonData from "./src/resources/data.json";
+import React, { useState } from 'react';
+import AppContainer from './src/routes';
+import { DataContext } from './src/components/data';
+import jsonData from './src/resources/data.json';
 
-export default function App() {
-  const [data, setData] = useState(jsonData);
+export default function App () {
+    const [data, setData] = useState(jsonData);
 
-  const setTasks = (tasks) => {
-    setData({
-      ...data,
-      tasks,
-    });
-  };
-
-  return (
-    <DataContext.Provider value={{ data, setData, setTasks }}>
-      <AppContainer />
-    </DataContext.Provider>
-  );
+    return (
+        <DataContext.Provider value={{ data, setData }}>
+            <AppContainer />
+        </DataContext.Provider>
+    );
 }
