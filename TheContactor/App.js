670a3be3263cefa23d5073c064c811a57c6fc084
sindrigunnars <102031392+sindrigunnars@users.noreply.github.com>
2023-12-04 11:51:29 +0000

    Remove conflict

--- TheContactor/App.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import AppContainer from './src/routes';
-
-export default function App () {
-    return (
-        <AppContainer />
-    );
-}
--- TheContactor/src/services/fileService.js
+++
@@ -1,6 +0,0 @@
-import * as FileSystem from 'expo-file-system';
-const contactDirectory = `${FileSystem.documentDirectory}contacts`;
-
-export const getDir = () => {
-    return contactDirectory;
-};
