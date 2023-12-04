da26875016d6ee0cc95c38d1ad1d0f43c1cf5fe5
sindrigunnars <102031392+sindrigunnars@users.noreply.github.com>
2023-12-04 11:47:58 +0000

    Add setting import contacts

---
+++ TheContactor/src/views/Settings/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import * as fileService from '../../services/fileService';
+import TextButton from '../../components/TextButton';
+import { SafeAreaView, StyleSheet, ScrollView } from 'react-native';
+const Settings = ({ navigation: { navigate } }) => {
+    return (
+        <SafeAreaView>
+            <ScrollView>
+                <TextButton text='Import Contacts' onPressFunc={() => fileService.importContacts()}/>
+            </ScrollView>
+        </SafeAreaView>
+    );
+};
+
+Settings.propTypes = {
+    navigation: PropTypes.shape({
+        navigate: PropTypes.func.isRequired
+    }).isRequired
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'center'
+    },
+    scrollContainer: {
+        paddingHorizontal: 20
+    }
+});
+
+export default Settings;
