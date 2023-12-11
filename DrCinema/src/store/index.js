6b3b4625883eb115cf9be537dc5d96d583735105
ThordisME <112634480+ThordisME@users.noreply.github.com>
2023-12-11 10:49:33 +0000

    upcoming now in store

--- DrCinema/src/store/index.js
+++ DrCinema/src/store/index.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import moviesSlice from '../slices/moviesSlice';
 import theatersSlice from '../slices/theatersSlice';
+import upcomingSlice from '../slices/upcomingSlice';
 
 export const store = configureStore({
     reducer: {
         movies: moviesSlice,
-        theaters: theatersSlice
+        theaters: theatersSlice,
+        upcoming: upcomingSlice
     }
 });
 
