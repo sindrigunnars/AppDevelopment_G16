aa78a46d850998092516e5bda308f71995c1161a
sindrigunnars <102031392+sindrigunnars@users.noreply.github.com>
2023-12-11 13:36:02 +0000

    Detail page for theatres, Main buttons open details, details button opens movies

--- DrCinema/src/routes/index.js
+++ DrCinema/src/routes/index.js
@@ -3,6 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Main from '../views/main';
 import Movies from '../views/movies';
+import TheatreDetail from '../views/theatreDetail';
 const Stack = createStackNavigator();
 
 const Routes = () => {
@@ -10,12 +11,16 @@ const Routes = () => {
         <NavigationContainer>
             <Stack.Navigator initialRouteName="Main">
                 <Stack.Screen
-                    name="Main"
+                    name="Theatres"
                     component={Main}
                 />
                 <Stack.Screen
                     name="Movies"
                     component={Movies}
+                />
+                <Stack.Screen
+                    name="Theatre"
+                    component={TheatreDetail}
                 />
             </Stack.Navigator>
         </NavigationContainer>
---
+++ DrCinema/src/views/theatreDetail/index.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { useNavigation } from '@react-navigation/native';
+// import { getToken } from '../../services/apiService';
+import {
+    Text,
+    View,
+    Pressable,
+    ScrollView,
+    SafeAreaView,
+    StyleSheet
+} from 'react-native';
+
+const TheatreDetail = ({ route, navigation: { navigate } }) => {
+    const data = route.params.data;
+    const { name, description, city, phone, website } = data;
+    const address = data['address\t'];
+
+    const navigation = useNavigation();
+
+    const removeSubstrings = (inputString, substringsToRemove) => {
+        const escapedSubstrings = substringsToRemove.map(substring => substring.replace(/[-\\^$*+?.()|[\]{}]/g, '\\$&')).join('|');
+        const regex = new RegExp(escapedSubstrings, 'g');
+        return inputString.replace(regex, '');
+    };
+
+    const parsedDescription = description ? removeSubstrings(description, ['<b>', '<br>']) : null;
+
+    return (
+        <SafeAreaView style={styles.container}>
+            <ScrollView style={styles.scrollContainer}>
+                <Pressable
+                    style= {{ rowGap: 10, marginBottom: 10, borderWidth: 1, borderColor: 'black', padding: 10, flexGrow: 1 }}
+                    onPress={() => navigation.navigate('Movies')}>
+                    <Text style={{ alignSelf: 'center' }}>{name}</Text>
+                    {parsedDescription ? <Text style={{ alignSelf: 'center', textAlign: 'justify' }}>{parsedDescription.trim()}</Text> : null }
+                    <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'space-between' }}>
+                        <Text>{address} {city}</Text>
+                        {phone ? <Text>{phone}</Text> : null }
+                        <Text>{website}</Text>
+                    </View>
+                </Pressable>
+            </ScrollView>
+        </SafeAreaView>
+    );
+};
+
+TheatreDetail.propTypes = {
+    navigation: PropTypes.shape({
+        navigate: PropTypes.func.isRequired
+    }).isRequired,
+    route: PropTypes.object.isRequired
+};
+
+export default TheatreDetail;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'center'
+    },
+    scrollContainer: {
+        paddingHorizontal: 20
+    }
+});
