bbfb2f53601386efebba2b7eaba606640812a626
ThordisME <112634480+ThordisME@users.noreply.github.com>
2023-12-12 15:45:30 +0000

    stuff

--- DrCinema/src/components/upcoming/index.js
+++ DrCinema/src/components/upcoming/index.js
@@ -1,29 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useNavigation } from '@react-navigation/native';
 import {
     Text,
     Image,
     View,
-    Pressable
+    TouchableOpacity
 } from 'react-native';
 
 const Upcoming = ({ data }) => {
+    const navigation = useNavigation();
     const title = data.title;
     const thumbnail = data.poster;
     const releaseDate = data['release-dateIS'] || 'Release-Date Unknown.';
 
-    // <Image style={{ alignSelf: 'center', width: '100px' }} source={{ uri: thumbnail }} resizeMode="contain" />
-    // <Image style={{ width: '100%', height: '100%' }} source={{ uri: { thumbnail } }} />
     return (
-        <Pressable
-            style= {{ rowGap: 10, marginBottom: 10, borderWidth: 1, borderColor: 'black', padding: 10 }}>
+        <TouchableOpacity
+            onPress={() => navigation.navigate('Upcoming', { data })}
+            style={{ rowGap: 10, marginBottom: 10, borderWidth: 1, borderColor: 'black', padding: 10 }}
+        >
             <Text style={{ alignSelf: 'center' }}>{title}</Text>
             <View>
                 <Image style={{ alignSelf: 'center', width: 100, height: 100 }} source={{ uri: thumbnail }} />
             </View>
             <Text style={{ alignSelf: 'center' }}>{ releaseDate }</Text>
-            { /* <Image style={{ alignSelf: 'center' }} source={{ uri: thumbnail }} /> */ }
-        </Pressable>
+        </TouchableOpacity>
     );
 };
 
