bdb74c079cbbfa4dbe811f7963f3ed9be22ada31
jonbjarki <jonbjarki02@gmail.com>
2023-12-13 16:42:49 +0000

    added showtimes and link to buy ticket to theatre movies

--- DrCinema/src/views/MovieDetail/index.js
+++ DrCinema/src/views/MovieDetail/index.js
@@ -1,25 +1,42 @@
 import { Text, Image, View, StyleSheet, ScrollView } from "react-native";
 import PropTypes from 'prop-types';
-
+import Showtimes from '../../components/Showtimes';
 
 const MovieDetail = ({ route, navigation: { navigate } }) => {
     const data = route.params;
-    const { id, title, poster, year, genres, durationMinutes, plot } = data;
+    const { id, title, poster, year, genres, durationMinutes, plot, showtimes, theatreId } = data;
+
+    let theatreShowtimes;
+    let theatre;
+    if (theatreId) {
+        theatre = showtimes.find(showtime => showtime.cinema.id === theatreId);
+        if (theatre) {
+            theatreShowtimes = theatre.schedule;
+        }
+    }
+
 
     return (
-        <ScrollView contentContainerStyle={styles.container}>
-            <Image style={styles.image} source={{ uri: poster }} />
-            { typeof(genres[0]) != 'number' ?
-            (<View style={styles.genres}>
-                {genres.map((genre, key) =>
-                    <View style={styles.genre} key={key}><Text>{genre["NameEN\t"] ? genre["NameEN\t"] : genre["Name"] }</Text></View>)}
-            </View>) : null }
-            <View style={styles.info}>
-                <Text>{year} • {durationMinutes}min</Text>
+        <View style={{ flex: 1, height: "100%" }}>
+            <ScrollView>
+                <View style={styles.container}>
+                    <Image style={styles.image} source={{ uri: poster }} />
+                    {typeof (genres[0]) != 'number' ?
+                        (<View style={styles.genres}>
+                            {genres.map((genre, key) =>
+                                <View style={styles.genre} key={key}><Text>{genre["NameEN\t"] ? genre["NameEN\t"] : genre["Name"]}</Text></View>)}
+                        </View>) : null}
+                    <View style={styles.info}>
+                        <Text>{year} • {durationMinutes}min</Text>
+                    </View>
+                    <Text style={styles.title}>{title}</Text>
+                    <Text style={styles.plot}>{plot}</Text>
+                    {/* Divider */}
+                    {theatreShowtimes ? <View style={styles.divider} /> : null}
+                    {theatreShowtimes ? <Showtimes times={theatreShowtimes} theatre={theatre.cinema.name} /> : null}
                 </View>
-            <Text style={styles.title}>{title}</Text>
-            <Text style={styles.plot}>{plot}</Text>
-        </ScrollView>
+            </ScrollView>
+        </View>
     )
 
 };
@@ -27,15 +44,14 @@ const MovieDetail = ({ route, navigation: { navigate } }) => {
 const styles = StyleSheet.create({
     image: {
         width: '100%',
-        height: '60%',
+        height: 400,
         resizeMode: 'contain',
     },
     container: {
-        height: '100%',
-        paddingTop: 10,
-        width: '100%',
+        flex: 1,
         alignItems: 'center',
-        paddingBottom: 50,
+        width: '100%',
+        paddingTop: 10,
     },
     movieContainer: {
         width: '70%',
@@ -67,6 +83,12 @@ const styles = StyleSheet.create({
     },
     plot: {
         width: '75%',
+    },
+    divider: {
+        borderBottomColor: 'black',
+        borderBottomWidth: StyleSheet.hairlineWidth,
+        marginVertical: 20,
+        width: '90%',
     }
 });
 
