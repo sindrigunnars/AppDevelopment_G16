1f09aad3a582297cbd766734d25af75a21e4cf4b
ThordisME <112634480+ThordisME@users.noreply.github.com>
2023-12-14 16:59:17 +0000

    now when you click on the movie you get to see where it's shown and you can by a ticket :)

--- DrCinema/src/views/MovieDetail/index.js
+++ DrCinema/src/views/MovieDetail/index.js
@@ -2,18 +2,40 @@ import React from 'react';
 import { Text, Image, View, StyleSheet, ScrollView } from 'react-native';
 import PropTypes from 'prop-types';
 import Showtimes from '../../components/Showtimes';
+import MultipleTheatresShowtimes from '../../components/MultipleTheatresShowtimes';
 
 const MovieDetail = ({ route, navigation: { navigate } }) => {
     const data = route.params;
     const { title, poster, year, genres, durationMinutes, plot, showtimes, theatreId } = data;
 
+    // movie details when clicked on 'all movies' screen
+    const allShowtimesByTheatre = {};
+    let theatres = [];
+    // movie details when clicked on 'theatre' screen
     let theatreShowtimes;
     let theatre;
+
     if (theatreId) {
         theatre = showtimes.find(showtime => showtime.cinema.id === theatreId);
         if (theatre) {
             theatreShowtimes = theatre.schedule;
         }
+    } else {
+        theatres = showtimes.map((showtime) => showtime.cinema.name);
+
+        showtimes.forEach(showtime => {
+            const name = showtime.cinema.name;
+
+            if (!allShowtimesByTheatre[name]) {
+                // schedule for that theatre
+                theatre = showtimes.find(showtime => showtime.cinema.name === name);
+                theatreShowtimes = theatre.schedule;
+                allShowtimesByTheatre[name] = {
+                    showtimes: theatreShowtimes
+                };
+            }
+        });
+        theatreShowtimes = null; // there was an issue with the component for this rendering as well
     }
 
     return (
@@ -34,6 +56,15 @@ const MovieDetail = ({ route, navigation: { navigate } }) => {
                     <Text style={styles.plot}>{plot}</Text>
                     {theatreShowtimes ? <View style={styles.divider} /> : null}
                     {theatreShowtimes ? <Showtimes times={theatreShowtimes} theatre={theatre.cinema.name} /> : null}
+                    {allShowtimesByTheatre ? <View style={styles.divider} /> : null}
+                    {allShowtimesByTheatre
+                        ? (
+                            <View>
+                                <MultipleTheatresShowtimes times={allShowtimesByTheatre} theatres={theatres} />
+                            </View>
+                        )
+                        : null
+                    }
                 </View>
             </ScrollView>
         </View>
